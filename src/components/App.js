61f2f3c9ea7d2a7a3a23e507e2e4c7aa42c2b16c
Vlad Chikov <chikovvlad@yandex.com>
2022-07-19 00:17:20 +0300

    выполнить 4 пункт ПР10

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,13 @@
 import Header from "./Header";
-import editProfileImgButton from "../images/editProfileImg-button.svg";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="page">
       <Header />
-      <main>
-        <section className="profile">
-          <div className="profile__avatar-container">
-            <img src="#" alt="Фото профиля" className="profile__avatar" />
-            <div className="profile__avatar-overlay">
-              <img
-                src={editProfileImgButton}
-                alt="Карандаш"
-                className="profile__editImg-button"
-              />
-            </div>
-          </div>
-          <div className="profile__info">
-            <h1 className="profile__title"></h1>
-            <p className="profile__subtitle"></p>
-            <button className="profile__edit-button" type="button"></button>
-          </div>
-          <button className="profile__add-button" type="button"></button>
-        </section>
-
-        <section className="elements">
-          <ul className="elements__list">
-            <template className="card">
-              <article className="element">
-                <img className="element__image" src="#" alt="" />
-                <button
-                  className="element__basket-button"
-                  type="button"
-                ></button>
-                <div className="element__caption">
-                  <h2 className="element__title"></h2>
-                  <div className="element__like-container">
-                    <button
-                      className="element__like-button"
-                      type="button"
-                      id="like"
-                    ></button>
-                    <p className="element__like-count"></p>
-                  </div>
-                </div>
-              </article>
-            </template>
-          </ul>
-        </section>
-      </main>
-      <footer className="footer">
-        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
-      </footer>
+      <Main />
+      <Footer />
       {/* Попап редактирования профиля */}
       <div className="popup popup_named_profile">
         <div className="popup__container">
